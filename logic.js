35066ea2ac6483466d7aae9d9a3d9b9817b39484
Rosa Garcia <rmga2106@gmail.com>
2021-08-22 21:11:08 -0500

    feat: pintar cada pokemon

--- logic.js
+++ logic.js
@@ -1,59 +1,40 @@
 const limit = 10;
 const url_limit = `https://pokeapi.co/api/v2/pokemon?offset=${limit}&limit=${limit}`;
 // const url = 'https://pokeapi.co/api/v2/pokemon/56';
+let arrPokes = [];
 window.onload = getAllData(url_limit);
 
 function getAllData (url, contextNode='') {
-    let detail;
     fetch(url)
     .then(result => result.json())
     .then(data => {
         if (data.results) {
-            detail = drawPokemon(data.results);
+            data.results.forEach(poke => {
+                getAllData(poke.url)
+            });
         } else {
-            detail = getInfoPokemon(data, contextNode);
-            console.log('detail tiene URL: ', detail);
+            drawPokemon(data); // obj de info por cada pokemon
+            arrPokes.push(data); 
         }
-        // detail = (data.results) ? drawPokemon(data.results) : getInfoPokemon(data);
-        return detail;
     })
 }
 
-function drawPokemon(pokemons) {
+function drawPokemon(pokemon) {
     const container = document.getElementById('containerPokemon');
-    let namePokeNode = '';
-    pokemons.forEach(poke => {
-        // <img src="getAllData(${poke.url})"/>
-        namePokeNode += `
-        <div data-url="${poke.url}" onpopstate=getAllData('${poke.url}', this); class="poke-container" onclick=selectedName(this,'${poke.name}')>
-            <p>${poke.name}</p>
-        </div>
-        `;
-    });
-    container.innerHTML = namePokeNode;
-    // getAllData(url_poke, contextNode);
-    return 
+    let namePokeNode = document.createElement("div");
+    let url_img = pokemon['sprites']['front_default'];
+    namePokeNode.innerHTML =`
+    <div id="${pokemon.name}" class="poke-container" onclick="selectedName('${pokemon.name}', this)">
+        <img src="${url_img}" alt="img-${pokemon.name}"/>
+        <p>${pokemon.name}</p>
+    </div>`;
+    container.appendChild(namePokeNode);
+    return
 }
 
-function getInfoPokemon(infoPokemon, contextNode) {
-    // console.log('this: ', contextNode);
-    let url_img = infoPokemon['sprites']['front_default'];
-    let node = document.createElement("img");
-    node.setAttribute("src", url_img);
-    node.setAttribute("alt", 'imagen');
-    contextNode.appendChild(node);
-    return url_img;
-}
-
-const selectedName = (contextNode,pokeName) => {
-    console.log('name: ', pokeName);
+const selectedName = (pokeInfo, contextNode='') => {
+    console.log('name: ', pokeInfo);
     console.log('contextNode: ', contextNode);
-    let url_poke = contextNode.getAttribute('data-url');
-    console.log('url_poke: ', url_poke);
-    getAllData(url_poke, contextNode);
-    // let detailData;
-    // setTimeout(function () {detailData = getAllData(url_poke)}, 4000);
-    // console.log("url-img**", detailData);
-    // getAllData(url_poke);
+    console.log('array:  ', arrPokes);
 }
 
